a3573e34b71dbd7762c5fb350901f1146f05e925
Deshawa <deshawaethugala77@gmail.com>
2026-08-28 22:32:38 +0530

    feat: improve MongoDB task schema and persistence

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
+
     email: {
       type: String,
       required: true,
@@ -14,6 +15,7 @@ const userSchema = new mongoose.Schema(
       lowercase: true,
       trim: true,
     },
+
     password: {
       type: String,
       required: true,
