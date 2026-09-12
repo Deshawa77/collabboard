e0ab08d1d2661e1eb7f0379febde3d8da5900b4e
Deshawa <deshawaethugala77@gmail.com>
2026-09-12 20:53:46 +0530

    ci: add GitHub Actions test pipeline

--- server/tests/setup.js
+++ server/tests/setup.js
@@ -8,15 +8,23 @@ let mongoServer;
 jest.setTimeout(60000);
 
 beforeAll(async () => {
-  mongoServer = await MongoMemoryServer.create({
-    binary: {
-      version: "7.0.14",
-      systemBinary:
-        "C:\\Users\\ROG\\.cache\\mongodb-binaries\\mongodb-7.0.14\\mongodb-win32-x86_64-windows-7.0.14\\bin\\mongod.exe",
-    },
-  });
+  let mongoUri;
 
-  const mongoUri = mongoServer.getUri();
+  if (process.env.MONGO_URI) {
+    // GitHub Actions / external MongoDB service
+    mongoUri = process.env.MONGO_URI;
+  } else {
+    // Local development: use the installed MongoDB binary
+    mongoServer = await MongoMemoryServer.create({
+      binary: {
+        version: "7.0.14",
+        systemBinary:
+          "C:\\Users\\ROG\\.cache\\mongodb-binaries\\mongodb-7.0.14\\mongodb-win32-x86_64-windows-7.0.14\\bin\\mongod.exe",
+      },
+    });
+
+    mongoUri = mongoServer.getUri();
+  }
 
   await mongoose.connect(mongoUri);
 });
